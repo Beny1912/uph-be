69fe37a8baed2faba8f65b48e4bb419f1e052cb9
Beny1912 <beny.reading@gmail.com>
2022-03-13 19:25:21 +0100

    add env config to mode bot or api

--- env/.env.pro
+++ env/.env.pro
@@ -1,5 +1,7 @@
 # ---------------- BASIC ENVIRONMENT CONFIGURATION ---------------------------
 NODE_ENV=PRO
 # ----------------------------------------------------------------------------
-
+# If you run in mode bot(BOT=YES)
+BOT=NO
+PORT=3000
 MONGO_URI=mongodb+srv://beny:polencito@cluster0.ha48h.mongodb.net/uphold?retryWrites=true&w=majority
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import connect from "./connect";
 import { db } from "./config/config";
 import path from "path";
 import Environment from "./helpers/tools/environment";
+import app from "./app";
 
 //Config Env
 Environment.config({
@@ -11,5 +12,14 @@ Environment.config({
 //Connect Database
 connect(db);
 
-// if API was a WebSocket or SSE i won't use a setInterval
-Bot.intervalAllTickerByCurrency(0.01, "USD", true, [], 5000);
+if (process.env.BOT === "YES") {
+  // if API was a WebSocket or SSE i won't use a setInterval
+  Bot.intervalAllTickerByCurrency(0.01, "USD", true, [], 5000);
+} else {
+  const PORT = process.env.PORT || 3000;
+  // start the express server
+  app.listen(PORT, (): void => {
+    // tslint:disable-next-line:no-console
+    console.log(`server started at http://localhost:${PORT}`);
+  });
+}
